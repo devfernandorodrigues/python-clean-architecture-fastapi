69c69d0e46ea6ab2d17ecd05966913c57f65c6eb
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2023-03-10 12:17:46 -0300

    feat: add todo domain

---
+++ app/domain/todo.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class Todo:
+    title: str
+    is_done: bool
+    uuid: str | None = None
+
+
+    def done(self):
+        self.is_done = True
+
+    def not_done(self):
+        self.is_done = False
---
+++ tests/unit/domain/test_todo.py
@@ -0,0 +1,17 @@
+from app.domain.todo import Todo
+
+def test_done(faker):
+    todo = Todo(
+        title=faker.pystr(),
+        is_done=False,
+    )
+    todo.done()
+    assert todo.is_done is True
+
+def test_not_done(faker):
+    todo = Todo(
+        title=faker.pystr(),
+        is_done=True,
+    )
+    todo.not_done()
+    assert todo.is_done is False
